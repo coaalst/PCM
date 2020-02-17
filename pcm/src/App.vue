da99211067b9b3b8ec7e8d7d5b54c7ebcac1622d
coaalst <coaalst98@gmail.com>
2020-02-17 18:31:47 +0100

    started work on the main page

--- pcm/src/App.vue
+++ pcm/src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <div id="app">
-
-    <router-view/>
+  <div id="app" style="margin-top: 0px">
+  <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
 }
 </script>
 
---
+++ pcm/src/components/Main.vue
@@ -0,0 +1,48 @@
+<template>
+<md-toolbar class="md-primary">
+  <div class="md-toolbar-row">
+    <div class="md-toolbar-section-start">
+      <h3 class="md-title">PCM</h3>
+    </div>
+    <div class="md-toolbar-section-end">
+
+      <div class="md-collapse">
+        <md-list>
+          <md-list-item href="#dashboard">
+            <i class="material-icons">dashboard</i>
+            <p class="hidden-lg hidden-md">Dashboard</p>
+          </md-list-item>
+          <md-list-item href="#dashboard">
+            <i class="material-icons">dashboard</i>
+            <p class="hidden-lg hidden-md">Dashboard</p>
+          </md-list-item>
+
+        </md-list>
+      </div>
+    </div>
+  </div>
+</md-toolbar>
+
+
+
+
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.md-primary{
+    background: #37996a;
+    color: white;
+    max-height: 50px;
+}
+.md-list{
+  flex-flow: row;  
+}
+</style>
