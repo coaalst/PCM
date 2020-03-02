b73b1b42cf2332899a85892dbb5d91d3d404b01c
Aleksandar Stojanovic <astojanovic17@raf.rs>
2020-03-02 17:52:27 +0100

    Reworked session impl

--- node/app.js
+++ node/app.js
@@ -1,33 +1,25 @@
 // Middleware
 const express = require('express');
 const session = require('express-session');
-const uuid = require('uuid/v4')
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const SQLiteStore = require('connect-sqlite3')(session);
+const app = express();
 
 // App setup
-const app = express();
 app.use(morgan('combine'));
 app.use(bodyparser.json());
 app.use(cors());
 
 app.use(session({
+    store: new SQLiteStore,
     name: 'dusko',
     secret: 'amburator',
-    genid: (req) => {
-        console.log('Server: ID sesije - ' + req.sessionID)
-        return uuid() // use UUIDs for session IDs
-    },
-    resave: false,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 3,
-        sameSite: true,
-        secure: false,
-    }
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }
 }));
-
-app.set('dusko', session.name);
 
 // Routes
 const pcRouter = require('./routes/pc.js');
@@ -42,15 +34,4 @@ app.use('/profiles/', profileRouter);
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log('Server started on port 4000');
-});
-
-// Base
-app.get('/status', (req, res) => {
-    res.send({ Status: "Online", Hotel: "Trivago" });
-});
-
-// Base
-app.get('/', (req, res) => {
-    if (req.session.userId) res.redirect('http://localhost:8080/#/main');
-    else res.redirect('http://localhost:8080/#/login');
 });
